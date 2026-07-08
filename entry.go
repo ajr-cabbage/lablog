2c8b80c21cec53bd36fe6319e0d4674b85b84089
ajr-cabbage <alexander.rakotz@gmail.com>
2026-07-08 01:19:18 -0400

    start implementing custom delegate

--- entry.go
+++ entry.go
@@ -1,16 +1,21 @@
 package main
 
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/bubbles/list"
+)
+
 type Entry struct {
 	friendlyName string
 	hostName     string
-	desccription string
+	description string
 	ipAddress    string
 	online       bool
 }
 
 // implement list.Item interface
 func (e Entry) FilterValue() string {
-	return e.friendlyName
+	return e.friendlyName + " " + e.hostName + e.ipAddress
 }
 
 func (e Entry) Title() string {
@@ -18,5 +23,19 @@ func (e Entry) Title() string {
 }
 
 func (e Entry) Description() string {
-	return e.desccription
+	return e.description
 }
+// implement DefaultDelegate interface
+type CustomDelegate struct {}
+
+func (d CustomDelegate) Height() int {return 3}
+
+func (d CustomDelegate) Spacing() int {return 1}
+
+func (d CustomDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd {return nil}
+
+func (d CustomDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
+	
+}
+
+
